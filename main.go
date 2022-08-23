0cd77d60940e0db4b9d4ec879354dbef6afae32d
vinkaannisa <vinkaannisa@gmail.com>
2022-08-24 05:19:14 +0700

    task point 4

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"alterra/hitung"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Saya vinka Annisa")
+	fmt.Println("fiturA point 4")
+
+	hasilTambah := hitung.Tambah(9, 9)
+	fmt.Println(hasilTambah)
 }
