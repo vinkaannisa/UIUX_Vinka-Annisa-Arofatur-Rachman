41d5f730bfe4df3d24d3cd632518f005fd5564b6
vinkaannisa <vinkaannisa@gmail.com>
2022-08-24 05:22:28 +0700

    task point 5

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"alterra/hitung"
+	"alterra/minus"
 	"fmt"
 )
 
@@ -11,4 +12,11 @@ func main() {
 
 	hasilTambah := hitung.Tambah(9, 9)
 	fmt.Println(hasilTambah)
+
+	fmt.Println("featureB point 5")
+	hasilKali := hitung.Kali(9, 9)
+	fmt.Println(hasilKali)
+
+	hasilKurang := minus.Kurang(9, 9)
+	fmt.Println(hasilKurang)
 }
